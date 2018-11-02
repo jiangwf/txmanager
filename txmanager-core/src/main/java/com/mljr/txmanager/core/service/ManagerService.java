47a73fd3933275c3a19c04edf994bfbb37e9f42a
tianhe2018 <xunzhao3456>
2018-11-02 17:29:30 +0800

    netty通讯模块骨架搭建

---
+++ txmanager-core/src/main/java/com/mljr/txmanager/core/service/ManagerService.java
@@ -0,0 +1,56 @@
+package com.mljr.txmanager.core.service;
+
+import com.mljr.txmanager.common.model.TransactionGroup;
+import com.mljr.txmanager.common.model.TransactionItem;
+
+import java.util.List;
+
+/**
+ * @author: he.tian
+ * @time: 2018-11-02 16:53
+ */
+public interface ManagerService {
+
+    /**
+     * 保存事务组，在事务发起方调用
+     * @param transactionGroup
+     * @return
+     */
+    boolean saveTransactionGroup(TransactionGroup transactionGroup);
+
+    /**
+     * 往事务组添加事务
+     * @param transactionGroupId
+     * @param transactionItem
+     */
+    boolean addTransaction(String transactionGroupId, TransactionItem transactionItem);
+
+    /**
+     * 根据事务组id获取事务组的事务项信息
+     * @param transactionGroupId
+     * @return
+     */
+    List<TransactionItem> selectByTransactionGroupId(String transactionGroupId);
+
+    /**
+     * 在事务回滚或者事务组中事务项全部提交的时候删除事务组信息
+     * @param transactionGroupId
+     */
+    void removeTransactionGroup(String transactionGroupId);
+
+    /**
+     * 更新事务信息
+     * @param transactionItem
+     */
+    void updateTransactionItem(TransactionItem transactionItem);
+
+
+    /**
+     * 获取事务组状态
+     * @param transactionGroupId
+     * @return
+     */
+    int selectTransactionGroupStatus(String transactionGroupId);
+
+
+}
---
+++ txmanager-core/src/main/java/com/mljr/txmanager/core/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author: he.tian
+ * @time: 2018-11-02 16:52
+ */
+package com.mljr.txmanager.core.service;
